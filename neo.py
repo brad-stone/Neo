3d92bbf8f625a1f4097f35d5e8a378c05e61c1af
brad-stone <stonet1@stonefoundation.net>
2018-09-30 07:31:17 -0700

    Ready to paste from Windows

--- neo.py
+++ neo.py
@@ -1,11 +1,15 @@
 '''
-After creating virtualenv (e.g. virtualenv neo)
+Create virtualenv
     cd virtualenvs
     virtualenv neo
+Activate virtualenv
     source neo/bin/activate
 Install...
     pip install Flask
     pip install neo4j-driver
+Start up Neo4j
+    sudo service neo4j start
+    test at http://localhost:7474
 To run ...
     python neo.py
 '''
@@ -27,12 +31,30 @@ def get_db ():
         g.neo4j_db = driver.session ()
     return g.neo4j_db
 
+
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'neo4j_db'):
         g.neo4j_db.close()
 
+@app.route("/")
+def get_graph():
+    db = get_db()
+    testmovie = '"The Matrix"'
+    results = db.run("MATCH (m:Movie) "
+        "WHERE m.title =~ " + testmovie+ " "
+        "RETURN m")
+    return Response(results, mimetype="application/json")
 
+'''
+
+    results = '{"title": "The Matrix", "cast": [{"job": "acted", "role": ["Emil"], "name": "Emil Eifrem"}, {"job": "acted", "role": ["Agent Smith"], "name": "Hugo Weaving"}, {"job": "directed", "role": null, "name": "Andy Wachowski"}, {"job": "produced", "role": null, "name": "Joel Silver"}]}'
+'''
+
+
+
+'''
 @app.route("/")
 def get_graph():
     db = get_db()
@@ -56,6 +78,8 @@ def get_graph():
                 i += 1
             rels.append({"source": source, "target": target})
     return Response(dumps({"nodes": nodes, "links": rels}), mimetype="application/json")
+'''
+
 
 '''def hello():
     return "Hello World!"
